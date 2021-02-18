fb673a3acb5ffb9c68fcb7e3d6a68ebc7bc3dd4a
Eugeny Alekseenko <puncong@mail.ru>
2021-02-18 10:17:28 +1000

    отображается вся инфа

--- map/urls.py
+++ map/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('info/', views.info, name='info'),
     path('terms_of_use/', views.ternms_of_use, name='terms_of_use'),
     path('my_tags/', views.my_tags, name='my_tags'),
+    path('admin_tags/', views.admin_tags, name='admin_tags'),
 ]
--- map/views.py
+++ map/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render, redirect
 import json, io
 
 from .forms import TagForm, UnverifiedTagForm
-from map.models import Tag
+from map.models import Tag, UnverifiedTag
 
 
 # function to add to JSON
 def write_json(data, filename='static/tags/tags.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
+
+def clear_tags(filename='static/tags/tags.json'):
+    with io.open(filename, encoding='utf-8') as json_file:
+        data = json.load(json_file)
+
+        data['features'] = []
+
+    write_json(data)
 
 
 def add_tag(filename='static/tags/tags.json',
@@ -18,7 +26,8 @@ def add_tag(filename='static/tags/tags.json',
             name='',
             description='',
             location='',
-            user=''
+            user='',
+            image='no_img'
             ):
     with io.open(filename, encoding='utf-8') as json_file:
         data = json.load(json_file)
@@ -33,15 +42,15 @@ def add_tag(filename='static/tags/tags.json',
                   "coordinates": [coord_x, coord_y]},
              "properties": {
                   "balloonContentHeader":
-                      "<font size=3><b><div id='output_name'>" + name + "</div></b></font>",
+                      "<font size=3><b><div id='output_name'>"+name+"</div></b></font>",
                   "balloonContentBody":
-                      "<div>"+
-                      description+
-                      "</div>",
+                      "<div>"+description+"</div>"
+                      "<img src='"+image+"' height='150' width='200'> <br/>"
+                      "<div>"+location+"</div>",
 
                         
                   "balloonContentFooter":
-                      "<div>Автор</div>"
+                      "<div>"+user.username+"</div>"
                   }}
 
         # appending data
@@ -53,9 +62,16 @@ def add_tag(filename='static/tags/tags.json',
 def index(request):
 
     tags = Tag.objects.all()
+    clear_tags()
     for tag in tags:
-        pass
-    # ИНИЦИАЛИЗАЦИЯ JSONA
+        add_tag(coord_x=tag.x_coord,
+                coord_y=tag.y_coord,
+                tag_id=tag.id,
+                name=tag.name,
+                description=tag.description,
+                location=tag.location,
+                user=tag.user,
+                image=tag.image.url)
 
     if request.method == 'POST' and 'btnform1' in request.POST:
         form = TagForm(request.POST, request.FILES)
@@ -64,13 +80,6 @@ def index(request):
             tag.image = request.FILES['image']
             # tag.UserID = request.user !!!!!!!!!!!!
             tag.save()
-
-            add_tag(coord_x=tag.x_coord,
-                    coord_y=tag.y_coord,
-                    tag_id=tag.id,
-                    name=tag.name,
-                    description=tag.description)
-
 
             return render(request, 'include/tag_added.html')
         else:
@@ -112,3 +121,8 @@ def my_tags(request):
     tags = Tag.objects.filter(user=request.user)
 
     return render(request, 'my_tags.html', {'tags': tags})
+
+def admin_tags(request):
+    tags = UnverifiedTag.objects.all()
+
+    return render(request, 'admin_tags.html', {'tags': tags})
